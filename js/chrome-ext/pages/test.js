a09bca4b9b4eef3f4b0d27125f51e12e63f17541
Senthil Kumar M <m.senthil.000@gmail.com>
2017-10-01 16:21:20 -0700

    chrome extension - init

---
+++ js/chrome-ext/pages/test.js
@@ -0,0 +1,25 @@
+document.getElementById("test").addEventListener('click', () => {
+    console.log("Popup DOM fully loaded and parsed");
+
+    function modifyDOM() {
+        //You can play with your DOM here or check URL against your regex
+        console.log('Tab script:');
+        console.log($);
+        console.log(document.body);
+        return document.body.innerHTML;
+    }
+
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        console.log(url);
+    });
+
+    //We have permission to access the activeTab, so we can call chrome.tabs.executeScript:
+    chrome.tabs.executeScript({
+        code: '(' + modifyDOM + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+        console.log('Popup script:')
+        console.log(results[0]);
+    });
+});
